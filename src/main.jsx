947f53059aeb78f322ec5b7647863ad5d6cbdda2
Lemon-zeniye <endalklemon.abate@gmail.com>
2024-10-16 17:18:02 +0300

    add mantin table

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
+import "mantine-datatable/styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
